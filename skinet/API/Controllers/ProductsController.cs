0790fa1f34f49269c7a67302d9eb7b146e98b7ee
francisco-oro <francisco.oro@comunidad.unam.mx>
2024-06-17 13:38:43 -0600

    Added swagger documentation for the project

--- skinet/API/Controllers/ProductsController.cs
+++ skinet/API/Controllers/ProductsController.cs
@@ -1,4 +1,7 @@
+using API.Data;
+using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
@@ -6,15 +9,30 @@ namespace API.Controllers;
 [Route("api/[controller]/")]
 public class ProductsController: ControllerBase
 {
-    [HttpGet("[action]")]
-    public string GetProducts()
+    private readonly StoreContext _storeContext;
+    public ProductsController(StoreContext storeContext)
     {
-        return "this will be a list of products";
+        _storeContext = storeContext;
+    }
+
+
+    [HttpGet("[action]")]
+    public async  Task<ActionResult<List<Product>>> GetProducts()
+    {
+        var products = await _storeContext.Products.ToListAsync();
+        
+        return products; 
     }
 
     [HttpGet("[action]/{id}")]
-    public string GetProduct()
+    public async Task<ActionResult<Product>> GetProduct(Guid id)
     {
-        return "this will be a product";
+        var product = await _storeContext.Products.FindAsync(id);
+        if (product == null)
+        {
+            return NotFound();
+        }
+
+        return product;
     }
 }
---
+++ skinet/API/Entities/Product.cs
@@ -0,0 +1,9 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Entities;
+
+public class Product
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+}
