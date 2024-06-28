47547297916caccef416bdfde38c7522379377d1
francisco-oro <francisco.oro@comunidad.unam.mx>
2024-06-27 22:52:43 -0600

    Added navbar html code

--- skinet/client/src/app/app.module.ts
+++ skinet/client/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import {MegaMenuModule} from "primeng/megamenu";
+import {MenubarModule} from "primeng/menubar";
+import {NgOptimizedImage} from "@angular/common";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MegaMenuModule,
+    MenubarModule,
+    NgOptimizedImage
   ],
   providers: [],
   bootstrap: [AppComponent]
