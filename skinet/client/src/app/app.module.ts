84769580dd3767a61843a8db7e6097f7710b254e
francisco-oro <francisco.oro@comunidad.unam.mx>
2024-06-29 20:05:50 -0600

    Added SearchBar component

--- skinet/client/src/app/app.module.ts
+++ skinet/client/src/app/app.module.ts
@@ -1,23 +1,25 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import {NavBarComponent} from './components/nav-bar/nav-bar.component';
 import {MegaMenuModule} from "primeng/megamenu";
 import {MenubarModule} from "primeng/menubar";
 import {NgOptimizedImage} from "@angular/common";
-import { ButtonModule} from "primeng/button";
+import {ButtonModule} from "primeng/button";
 import {RippleModule} from "primeng/ripple";
 import {TagModule} from "primeng/tag";
-import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import {MenuBarComponent} from './components/menu-bar/menu-bar.component';
+import {SearchBarComponent} from './components/search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     MenuBarComponent,
+    SearchBarComponent,
   ],
   imports: [
     BrowserModule,
@@ -28,9 +30,11 @@ import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
     NgOptimizedImage,
     ButtonModule,
     RippleModule,
-    TagModule
+    TagModule,
+    MenubarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
